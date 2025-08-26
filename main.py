f3a855cb7cd9e5c47d8439aa5eb38c4ea9e98924
washim <mdwashimmomin04x@gmail.com>
2025-08-26 19:40:23 +0530

    p

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-print("hello world")
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend
+
 import matplotlib.pyplot as plt
+
+print("hello world")
 plt.plot([1, 2, 3], [4, 5, 1])
 plt.savefig('test.png')
+
 
 
